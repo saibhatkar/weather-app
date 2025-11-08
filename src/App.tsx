bee81a33bcd2e96f2aedec34df61626b605ca6a0
Sai Bhatkar <saibhatkar999@gmail.com>
2025-11-08 15:58:11 +0530

    install necessary shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div>Let's start building our weather app!</div>
+      <Button variant="destructive">Get Weather</Button>
     </>
   );
 }
